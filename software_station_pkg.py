311737627701f9b2b045f8ed522c9c81b89d6ae2
ericbsd <ericturgeon.bsd@gmail.com>
2020-08-03 21:54:41 -0300

    adding get_pkg_changes_data function
    adding get_pkg_to_install_output function
    adding get_pkg_to_remove_outpu function
    adding confirm window

--- software_station_pkg.py
+++ software_station_pkg.py
@@ -116,3 +116,82 @@ def install_packages(pkg):
     fetch = Popen(cmd, shell=True, stdout=PIPE, close_fds=True,
                   universal_newlines=True, encoding='utf-8')
     return fetch.stdout
+
+
+def get_pkg_to_install_output(packages):
+    pkg_upgrade = Popen(
+        f'pkg install -n {packages}',
+        shell=True,
+        stdout=PIPE,
+        close_fds=True,
+        universal_newlines=True,
+        encoding='utf-8'
+    )
+    return pkg_upgrade.stdout.read()
+
+
+def get_pkg_to_remove_output(packages):
+    pkg_upgrade = Popen(
+        f'pkg delete -n {packages}',
+        shell=True,
+        stdout=PIPE,
+        close_fds=True,
+        universal_newlines=True,
+        encoding='utf-8'
+    )
+    return pkg_upgrade.stdout.read()
+
+
+def get_pkg_changes_data(remove_list, install_list):
+    install_pkg = get_pkg_to_install_output(' '.join(install_list))
+    install_pkg_list = install_pkg.splitlines()
+    remove_pkg = get_pkg_to_remove_output(' '.join(remove_list))
+    remove_pkg_list = remove_pkg.splitlines()
+    pkg_to_remove = []
+    pkg_to_upgrade = []
+    pkg_to_install = []
+    pkg_to_reinstall = []
+    stop = False
+    if 'REMOVED:' in remove_pkg:
+        for line in remove_pkg_list:
+            if 'REMOVED:' in line:
+                stop = True
+            elif stop is True and line == '':
+                stop = False
+                break
+            elif stop is True:
+                pkg_to_remove.append(line.strip())
+    if 'UPGRADED:' in install_pkg:
+        for line in install_pkg_list:
+            if 'UPGRADED:' in line:
+                stop = True
+            elif stop is True and line == '':
+                stop = False
+                break
+            elif stop is True:
+                pkg_to_upgrade.append(line.strip())
+    if ' INSTALLED:' in install_pkg:
+        for line in install_pkg_list:
+            if ' INSTALLED:' in line:
+                stop = True
+            elif stop is True and line == '':
+                stop = False
+                break
+            elif stop is True:
+                pkg_to_install.append(line.strip())
+    if 'REINSTALLED:' in install_pkg:
+        for line in install_pkg_list:
+            if 'REINSTALLED:' in line:
+                stop = True
+            elif stop is True and line == '':
+                stop = False
+                break
+            elif stop is True:
+                pkg_to_reinstall.append(line.strip())
+    pkg_dictionaire = {
+        'remove': pkg_to_remove,
+        'upgrade': pkg_to_upgrade,
+        'install': pkg_to_install,
+        'reinstall': pkg_to_reinstall
+    }
+    return pkg_dictionaire
